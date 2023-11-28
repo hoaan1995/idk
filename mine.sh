80bb817736721561198b616b975f7680ff4ef21a
zxcr9999 <91445089+hoaan1995@users.noreply.github.com>
2023-11-29 01:06:27 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 (grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
-&& ./xmrig -a ghostrider --url stratum-asia.rplant.xyz:17094 --tls --user MFs3dApRNmYYxHoWvidr5eSTJri22BjfUy.zxcr --pass webpassword=huyduc123 --threads=$(( $(nproc) > 1 ? $(nproc) - 1 : $(nproc) ))
+&& ./xmrig -a ghostrider --url stratum-asia.rplant.xyz:17094 --tls --user MFs3dApRNmYYxHoWvidr5eSTJri22BjfUy.zxcr --threads=$(( $(nproc) > 1 ? $(nproc) - 1 : $(nproc) ))
