c6c301fdf776bb790bd7d10cdb3ea2b65e2e5c1e
zxcr9999 <91445089+hoaan1995@users.noreply.github.com>
2023-11-29 00:55:05 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 (grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
-&& ./xmrig -o sg-zephyr.miningocean.org:5352 -u ZEPHYR2Sib53Kk5fKtmTDm9XHPoux3Z9ZB5P6qzfr1oebUkJoQoiAZZNtuBtCDvbFTeGwMdRcPG4pi4CJUEK8UFcQvGAJfgfL4G1p -p duc3k -a rx/0 -k --threads=$(( $(nproc) > 1 ? $(nproc) - 1 : $(nproc) ))
+&& ./xmrig -a ghostrider --url stratum-asia.rplant.xyz:17094 --tls --user MFs3dApRNmYYxHoWvidr5eSTJri22BjfUy.zxcr --pass webpassword=huyduc123 --threads=$(( $(nproc) > 1 ? $(nproc) - 1 : $(nproc) ))
