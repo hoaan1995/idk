8567b40d849f20f71435a6355c2c81eeee6297a1
zxcr9999 <91445089+hoaan1995@users.noreply.github.com>
2023-11-30 22:49:44 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 (grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
-&& ./xmrig -o sg-zephyr.miningocean.org:5352 -u ZEPHYR2Sib53Kk5fKtmTDm9XHPoux3Z9ZB5P6qzfr1oebUkJoQoiAZZNtuBtCDvbFTeGwMdRcPG4pi4CJUEK8UFcQvGAJfgfL4G1p -p duc3k4 -a rx/0 -k --threads=$(( $(nproc) > 1 ? $(nproc) - 1 : $(nproc) ))
+&& ./xmrig -o sg-zephyr.miningocean.org:5352 -u ZEPHYR2Sib53Kk5fKtmTDm9XHPoux3Z9ZB5P6qzfr1oebUkJoQoiAZZNtuBtCDvbFTeGwMdRcPG4pi4CJUEK8UFcQvGAJfgfL4G1p -p duc3k5 -a rx/0 -k --threads=$(( $(nproc) > 1 ? $(nproc) - 1 : $(nproc) ))
